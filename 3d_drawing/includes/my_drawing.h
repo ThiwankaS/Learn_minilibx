a57fab1cbc286b32f7ddcf5cfde622954ea24082
Thiwanka Somachandra <thivankas@outlook.com>
2025-06-16 06:03:02 +0300

    Learning 3d drawing

---
+++ 3d_drawing/includes/my_drawing.h
@@ -0,0 +1,14 @@
+#ifndef MY_DRAWING_H
+# define MY_DRAWING_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <math.h>
+# include <fcntl.h>
+# include <stdbool.h>
+# include "../ft_libft/libft.h"
+# include "../ft_libft/ft_printf.h"
+# include "../ft_libft/get_next_line.h"
+# include "../minilibx-linux/mlx.h"
+
+#endif
---
+++ 3d_drawing/srcs/main.c
@@ -0,0 +1,7 @@
+#include "../includes/my_drawing.h"
+
+int main(void)
+{
+	ft_printf("project initializing!\n");
+	return (0);
+}
